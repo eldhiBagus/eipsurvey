e89ae4cef066d03788f5edab1b5b642253fc7778
eldhiBagus <baguseldhi@gmail.com>
2025-11-14 15:48:49 +0700

    copyright footer

--- application/views/umum/u_footer.php
+++ application/views/umum/u_footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-center small">
-            <div class="text-muted">Copyright &copy; Your Website 2022</div>
+            <div class="text-muted">Copyright &copy; Eco-Industrial Park 2025</div>
         </div>
     </div>
 </footer>
