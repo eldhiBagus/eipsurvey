35789a19aecf19ad6970cc66d49af13fd405228a
eldhiBagus <baguseldhi@gmail.com>
2025-11-17 11:13:15 +0700

    edit ucapan terima kasih

--- application/views/utama/thanks.php
+++ application/views/utama/thanks.php
@@ -1,12 +1,12 @@
 <div class="container mt-5 text-center">
     <h2 class="text-success">🎉 Terima kasih telah mengisi survei!</h2>
     <?php if (isset($next) && $next): ?>
-    <p class="mt-3">Lanjutkan ke survei berikutnya:</p>
-    <a href="<?php echo site_url('welcome/form/' . $next->slug); ?>" class="btn btn-primary btn-lg">
-        Lanjut ke <?php echo $next->title; ?> →
-    </a>
+        <p class="mt-3">Lanjutkan ke survei berikutnya:</p>
+        <a href="<?php echo site_url('welcome/form/' . $next->slug); ?>" class="btn btn-primary btn-lg">
+            Lanjut ke <?php echo $next->title; ?> →
+        </a>
     <?php else: ?>
-    <p class="mt-3">Anda telah menyelesaikan semua survei.</p>
-    <a href="<?php echo site_url('/'); ?>" class="btn btn-secondary btn-lg">Kembali ke Halaman Awal</a>
+        <p class="mt-3">Anda telah menyelesaikan pengisian survei.</p>
+        <a href="<?php echo site_url('/'); ?>" class="btn btn-secondary btn-lg">Kembali ke Halaman Awal</a>
     <?php endif; ?>
 </div>
